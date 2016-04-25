316c063646bf921c5ef4640c891b535260886c3b
deshrazz <deshraz@gmail.com>
2016-04-25 22:28:13 +0530

    Update Comments.php

--- runtime/modules/forum/models/Comments.php
+++ runtime/modules/forum/models/Comments.php
@@ -115,6 +115,233 @@ class Comments extends CActiveRecord
         
         }
         
+		public static function getPagination($viewLimitValue, $pageSelected, $commentOrderBy, $listingId, $userProfession)
+    {
+
+
+        $viewLimitValue = (isset($viewLimitValue)) ? $viewLimitValue : 6;
+
+        $viewOffset = (isset($viewOffset)) ? $viewOffset : 0;
+
+        $commentOrderBy = (isset($commentOrderBy)) ? $commentOrderBy : ' user_default_date_create DESC ';
+
+        $userProfession = (isset($userProfession)) ? $userProfession : 0;
+
+
+        $limit = $viewLimitValue;
+
+        $offset = $viewOffset;
+
+
+        if (($commentOrderBy != "user_default_reputation desc") && ($commentOrderBy != "user_default_reputation asc") && ($userProfession == "0")) {
+
+
+            $sql = "SELECT COUNT(*) as total_post";
+
+            $sql .= " FROM {{interactions}}";
+
+            $sql .= " WHERE user_default_first_interations = '1'";
+
+            $sql .= " AND user_default_listing_id = {$listingId}";
+
+            $sql .= " LIMIT {$offset}, {$limit}; ";
+
+
+            $totalPosts = Yii::app()->db->createCommand($sql)->queryAll();
+
+            $nbOfPost = (int)$totalPosts[0]['total_post'];
+
+
+        } elseif ((($commentOrderBy == "user_default_reputation desc") || ($commentOrderBy == "user_default_reputation asc")) && ($userProfession == "0")) {
+
+
+            $sql = "SELECT COUNT(c1.id) as total_post FROM {{interactions}} c1";
+
+            $sql .= " INNER JOIN (";
+
+            $sql .= " SELECT u.drg_id as user_id, (sum(likes_total) - sum(dislikes_total)) AS user_reputation";
+
+            $sql .= " FROM {{comments}} c2";
+
+            $sql .= " INNER JOIN {{user}} u";
+
+            $sql .= " ON c2.user_id = u.drg_id";
+
+            $sql .= " WHERE c2.listing_id = {$listingId}";
+
+            $sql .= " GROUP BY c2.user_id) z";
+
+            $sql .= " ON z.user_id = c1.user_id";
+
+            $sql .= " WHERE c1.first_comment = '1'";
+
+            $sql .= " AND c1.listing_id = {$listingId}";
+
+            $sql .= " LIMIT {$offset}, {$limit}; ";
+
+
+            $totalPosts = Yii::app()->db->createCommand($sql)->queryAll();
+
+            $nbOfPost = (int)$totalPosts[0]['total_post'];
+
+
+        } elseif (($commentOrderBy != "user_reputation desc") && ($commentOrderBy != "user_reputation asc") && ($userProfession != "0")) {
+
+
+            $sql = "SELECT COUNT(c.id) as total_post FROM {{comments}} c";
+
+            $sql .= " INNER JOIN {{user}} u";
+
+            $sql .= " ON c.user_id = u.drg_id";
+
+            $sql .= " INNER JOIN {{profession}} p";
+
+            $sql .= " ON u.drg_pstatus = p.profession_id";
+
+            $sql .= " WHERE c.first_comment = '1'";
+
+            $sql .= " AND c.listing_id = {$listingId}";
+
+            $sql .= " AND p.profession_id = {$userProfession}";
+
+            $sql .= " LIMIT {$offset}, {$limit}; ";
+
+
+            $totalPosts = Yii::app()->db->createCommand($sql)->queryAll();
+
+            $nbOfPost = (int)$totalPosts[0]['total_post'];
+
+
+        } elseif ((($commentOrderBy == "user_reputation desc") || ($commentOrderBy == "user_reputation asc")) && ($userProfession != "0")) {
+
+
+            $sql = "SELECT COUNT(c1.id) as total_post FROM {{comments}} c1";
+
+            $sql .= " INNER JOIN (";
+
+            $sql .= " SELECT u.drg_id as user_id, (sum(likes_total) - sum(dislikes_total)) AS user_reputation";
+
+            $sql .= " FROM {{comments}} c2";
+
+            $sql .= " INNER JOIN {{user}} u";
+
+            $sql .= " ON c2.user_id = u.drg_id";
+
+            $sql .= " INNER JOIN {{profession}} p";
+
+            $sql .= " ON u.drg_pstatus = p.profession_id";
+
+            $sql .= " WHERE c2.listing_id = {$listingId}";
+
+            $sql .= " AND p.profession_id = {$userProfession}";
+
+            $sql .= " GROUP BY c2.user_id) z";
+
+            $sql .= " ON z.user_id = c1.user_id";
+
+            $sql .= " WHERE c1.first_comment = '1'";
+
+            $sql .= " AND c1.listing_id = {$listingId}";
+
+            $sql .= " LIMIT {$offset}, {$limit}; ";
+
+
+            $totalPosts = Yii::app()->db->createCommand($sql)->queryAll();
+
+            $nbOfPost = (int)$totalPosts[0]['total_post'];
+
+
+        }
+
+
+        // All posts in one page
+
+        if ($nbOfPost <= $viewLimitValue) {
+
+
+            $paginationContent = "<a class='active'>Page 1</a>";
+
+
+        } else {
+
+
+            // Get the previous page if the select page isn't the first
+
+            if (($pageSelected != 1)) {
+
+
+                $previousPage = (int)($pageSelected - 1);
+
+
+                $offset = (int)(($previousPage - 1) * $viewLimitValue);
+
+
+                $paginationContent = "<a orderby='{$commentOrderBy}' offset='{$offset}' page='{$previousPage}'>< Previous</a>";
+
+
+            }
+
+
+            // Loop the pagination on depend the limit view
+
+            for ($i = 1; $i <= ($nbOfPost / $viewLimitValue); $i++) {
+
+
+                $activeClass = ($i == $pageSelected) ? "class='active'" : "";
+
+
+                $offset = (int)(($i - 1) * $viewLimitValue);
+
+
+                $paginationContent .= "<a orderby='{$commentOrderBy}' offset='{$offset}' page='{$i}' {$activeClass}>{$i}</a>";
+
+            }
+
+
+            // Get the next page link
+
+            if (($pageSelected * $viewLimitValue) < $nbOfPost) {
+
+
+                $nexPage = (int)($pageSelected + 1);
+
+
+                $offset = (int)(($nexPage - 1) * $viewLimitValue);
+
+
+                $koffset = (int)(($i - 1) * $viewLimitValue);
+
+
+                $activeClass = ($i == $pageSelected) ? "class='active'" : "";
+
+
+                $paginationContent .= "<a orderby='{$commentOrderBy}' offset='{$koffset}' {$activeClass} page='{$i}'>{$i}</a>";
+
+                $nextPagination = "<a orderby='{$commentOrderBy}' offset='{$offset}' page='{$nexPage}'>Next ></a>";
+
+
+            }
+
+
+            // Render the next page link
+
+            if (($pageSelected * $viewLimitValue) < $nbOfPost) {
+
+
+                $paginationContent .= $nextPagination;
+
+
+            }
+
+
+        }
+
+
+        return $paginationContent;
+
+
+    }
+		
 
         /**
          * List the comments
@@ -139,14 +366,15 @@ class Comments extends CActiveRecord
             if( ($commentOrderBy != "user_default_reputation desc") && ($commentOrderBy != "user_default_reputation asc") && ($userProfession == "0") ){
                 
                 $criteria = new CDbCriteria;
-                $criteria->select = array('*, (user_default_likes_total - user_default_dislikes_total) as rate');
-                $criteria->addCondition('user_default_listing_id = '.$listingId);
-                $criteria->addCondition("user_default_first_interations = '1'");
+                $criteria->select = array(' *, (user_default_likes_total - user_default_dislikes_total) as rate ');
+                $criteria->addCondition(' user_default_listing_id = '.$listingId );
+                $criteria->addCondition(" user_default_first_interations = '1' ");
+                $criteria->order = $commentOrderBy;
                 $criteria->offset = $offset;
                 $criteria->limit = $limit;
-                $criteria->order = $commentOrderBy;
-                
+               //$sql = "SELECT *, (user_default_likes_total - user_default_dislikes_total) as rate  from {{interactions}} where user_default_listing_id = $listingId and user_default_first_interations = '1' ORDER BY $commentOrderBy LIMIT $offset, $limit ";
                 $CDbCriteria = TRUE;
+				
                 
             }elseif ( (($commentOrderBy == "user_default_reputation desc") || ($commentOrderBy == "user_default_reputation asc")) && ($userProfession == "0") ) {
             
@@ -163,7 +391,6 @@ class Comments extends CActiveRecord
                 $sql .= " AND c1.user_default_listing_id = {$listingId}";
                 $sql .= " ORDER BY z.{$commentOrderBy}";
                 $sql .= " LIMIT {$offset}, {$limit}; ";
-                
             }elseif ( ($commentOrderBy != "user_default_reputation desc") && ($commentOrderBy != "user_default_reputation asc") && ($userProfession != "0") ) {
                 
                 $sql = "SELECT c.id, (user_default_likes_total - user_default_dislikes_total) as rate FROM {{interactions}} c";
@@ -176,7 +403,6 @@ class Comments extends CActiveRecord
                 $sql .= " AND p.profession_id = {$userProfession}";
                 $sql .= " ORDER BY {$commentOrderBy}";
                 $sql .= " LIMIT {$offset}, {$limit}; ";
-                
             }elseif ( (($commentOrderBy == "user_default_reputation desc") || ($commentOrderBy == "user_default_reputation asc")) && ($userProfession != "0") ) {
             
                 $sql = "SELECT c1.id, z.user_default_reputation FROM {{interactions}} c1";
@@ -195,9 +421,8 @@ class Comments extends CActiveRecord
                 $sql .= " AND c1.user_default_listing_id = {$listingId}";
                 $sql .= " ORDER BY {$commentOrderBy}";
                 $sql .= " LIMIT {$offset}, {$limit}; ";
-                
+               
             }
-            
             if( !$CDbCriteria ){
                                 
                 $commentsCommand = Yii::app()->db->createCommand($sql)->queryAll();
@@ -282,3 +507,5 @@ class Comments extends CActiveRecord
         }
 
 }
+
+?>
